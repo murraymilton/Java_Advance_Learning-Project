86b7198bad07c239bfd30269cc37f5aa571b4a85
murraymilton <murray.l.milton@outlook.com>
2022-05-16 17:00:34 +0200

    initial commit

---
+++ Learning_Java/src/HelloWorld.java
@@ -0,0 +1,21 @@
+public class HelloWorld {
+    public static void main(String[] args){
+//        Primitive Data Types
+        int studentAge = 15;
+        double studentGPA = 3.89;
+        String studentFirstName = "Murray";
+        String studentLastName = "Milton";
+        char studentFirstInitial = studentFirstName.charAt(0);
+        char studentLastInitial = studentLastName.charAt(0);
+        boolean hasPerfectAttendance = true;
+        System.out.println(studentAge);
+        System.out.println(studentGPA);
+        System.out.println(studentFirstInitial);
+        System.out.println(studentLastInitial);
+        System.out.println(hasPerfectAttendance);
+
+        String favoriteCar = "Bentley";
+
+
+    }
+}
