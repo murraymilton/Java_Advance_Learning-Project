60c154328f1899de204d15f89ed8f9974efcf75f
murraymilton <murray.l.milton@outlook.com>
2022-05-17 16:10:31 +0200

    added coding challenge for the student

--- Learning_Java/src/HelloWorld.java
+++ Learning_Java/src/HelloWorld.java
@@ -1,21 +1,12 @@
 public class HelloWorld {
     public static void main(String[] args){
 //        Primitive Data Types
-        int studentAge = 15;
-        double studentGPA = 3.89;
-        String studentFirstName = "Murray";
-        String studentLastName = "Milton";
-        char studentFirstInitial = studentFirstName.charAt(0);
-        char studentLastInitial = studentLastName.charAt(0);
-        boolean hasPerfectAttendance = true;
-        System.out.println(studentAge);
-        System.out.println(studentGPA);
-        System.out.println(studentFirstInitial);
-        System.out.println(studentLastInitial);
-        System.out.println(hasPerfectAttendance);
 
-        String favoriteCar = "Bentley";
 
+        int numb = 5;
+        for(int i = 1; i <= 10; i++){
+            System.out.println(numb * i);
+        }
 
     }
 }
