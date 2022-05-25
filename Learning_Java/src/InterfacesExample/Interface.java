dbd70ac18847938299bf7e2181effa57249cc1f7
murraymilton <murray.l.milton@outlook.com>
2022-05-25 10:41:37 +0200

    added the exception handling example

--- Learning_Java/src/InterfacesExample/Interface.java
+++
@@ -1,4 +0,0 @@
-package InterfacesExample;
-
-public class Interface {
-}
---
+++ Learning_Java/src/InterfacesExample/Vehicle.java
@@ -0,0 +1,11 @@
+package InterfacesExample;
+
+import java.net.DatagramSocket;
+
+public interface Vehicle {
+    public String getType();
+
+    public String getSpeed();
+
+    public String getColor();
+}
