5719e206ec01e23f9f7e19583794323b8dab6ef2
murraymilton <murray.l.milton@outlook.com>
2022-05-25 15:31:39 +0200

    added the Generics example to storing objects and returning classnames

--- Learning_Java/src/scannerExample/UserInputTester.java
+++ Learning_Java/src/scannerExample/UserInputTester.java
@@ -4,14 +4,16 @@ import java.util.Scanner;
 public class UserInputTester {
     public static void main(String[]args){
         Scanner scanner = new Scanner(System.in);
-        try{
+        try {
             System.out.println("Enter a String");
             String userInput = scanner.nextLine();
             System.out.println("User Input: " + userInput);
-        }catch (Exception ex){
+
+        } catch (Exception ex){
             ex.getLocalizedMessage();
         }finally {
             scanner.close();
         }
+
     }
 }
